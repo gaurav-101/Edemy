68f76f1b5ee3448255a4e6386b8dee28e703d391
Gaurav Agarwal <20bcs047@iiitdwd.ac.in>
2023-11-09 20:41:03 +0530

    init

---
+++ client/app/about/About.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { styles } from "../styles/style";
+
+const About = () => {
+  return (
+    <div className="text-black dark:text-white">
+      <br />
+      <h1 className={`${styles.title} 800px:!text-[45px]`}>
+        What is <span className="text-gradient">Edemy?</span>
+      </h1>
+      
+      <br />
+      <div className="w-[95%] 800px:w-[85%] m-auto">
+        <p className="text-[18px] font-Poppins">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+          <br />
+          <br />
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+          <br />
+          <br />
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+          <br />
+          <br />
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+          <br />
+          <br />
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+          <br />
+          <br />
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+          <br />
+          <br />
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+        </p>
+        <br />
+        <span className="text-[22px]">Harsh, Gaurav, Pruthvi, Anil</span>
+        <h5 className="text-[18px] font-Poppins">
+          Founder and CEO of Edemy
+        </h5>
+        <br />
+        <br />
+        <br />
+      </div>
+    </div>
+  );
+};
+
+export default About;
